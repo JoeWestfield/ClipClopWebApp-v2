b22d8db6bdf3eeb7d2205b4ebf700c50df427c6d
Richard Chau <richardhcchau@gmail.com>
2018-02-17 19:47:22 +0800

    test 02

--- app/src/main/java/hk/com/clipclop/clipclop_web_app/MainActivity.java
+++ app/src/main/java/hk/com/clipclop/clipclop_web_app/MainActivity.java
@@ -1,5 +1,7 @@
 package hk.com.clipclop.clipclop_web_app;
 
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
-    // test pull 02
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    // hahaha whywhywhya
+
+    @SafeVarargs
+    public void aVoid
 }
