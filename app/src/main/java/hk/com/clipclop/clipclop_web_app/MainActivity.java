b143cb0696fad067d023f8daa4f3b67c3a2f1b9c
Richard Chau <richardhcchau@gmail.com>
2018-02-17 19:12:41 +0800

    Initial Repo

---
+++ app/src/main/java/hk/com/clipclop/clipclop_web_app/MainActivity.java
@@ -0,0 +1,13 @@
+package hk.com.clipclop.clipclop_web_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
